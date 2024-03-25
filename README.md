9ceacf176b182b7b9d0174b96567f1828ff0dbd7
Dj <dmhalljr87@outlook.com>
2024-03-24 18:42:05 -0700

    Added category buttons

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # dmh34.github.io
 
-Rumble Icon ["https://icons8.com/icon/PK4azLeOpaNW/rumble"]
+Rumble Icon [https://icons8.com/icon/PK4azLeOpaNW/rumble]
+<a target="_blank" href="https://icons8.com/icon/114052/cancel">Close</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+<a target="_blank" href="https://icons8.com/icon/eIvodJbbBLyD/apple-arcade">Game</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+<a target="_blank" href="https://icons8.com/icon/3351/music">Music</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ portfolio/src/assests/index.tsx
@@ -0,0 +1,34 @@
+import { ReactComponent as AppIcon } from "../assests/AppIcon.svg";
+import { ReactComponent as GithubIcon } from "../assests/GithubIcon.svg";
+
+import { ReactComponent as EmailIcon } from "../assests/EmailIcon.svg";
+import { ReactComponent as LinkedinIcon } from "../assests/LinkedinIcon.svg";
+import { ReactComponent as CVIcon } from "../assests/CVIcon.svg";
+import { ReactComponent as YoutubeIcon } from "../assests/YoutubeIcon.svg";
+import { ReactComponent as RumbleIcon } from "../assests/RumbleIcon.svg";
+import { ReactComponent as TwitchIcon } from "../assests/TwitchIcon.svg";
+import { ReactComponent as TwitterIcon } from "../assests/TwitterIcon.svg";
+import { ReactComponent as InstagramIcon } from "../assests/InstagramIcon.svg";
+import { ReactComponent as FaceBookIcon } from "../assests/FaceBookIcon.svg";
+import { ReactComponent as CloseIcon } from "../assests/CloseIcon.svg";
+import { ReactComponent as GameIcon } from "../assests/GameIcon.svg";
+import { ReactComponent as MusicIcon } from "../assests/MusicIcon.svg";
+
+const Icons = {
+  AppIcon: <AppIcon />,
+  GithubIcon: <GithubIcon />,
+  CloseIcon: <CloseIcon />,
+  EmailIcon: <EmailIcon />,
+  LinkedinIcon: <LinkedinIcon />,
+  CVIcon: <CVIcon />,
+  YoutubeIcon: <YoutubeIcon />,
+  RumbleIcon: <RumbleIcon />,
+  TwichIcon: <TwitchIcon />,
+  TwitterIcon: <TwitterIcon />,
+  InstagramIcon: <InstagramIcon />,
+  FaceBookIcon: <FaceBookIcon />,
+  GameIcon: <GameIcon />,
+  MusicIcon: <MusicIcon />,
+};
+
+export default Icons;
