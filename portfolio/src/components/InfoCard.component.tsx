856ecb537b2e43483a88e7e63e23e93faa965536
Dj <dmhalljr87@outlook.com>
2024-03-28 14:11:00 -0700

    adjustments to the style

--- portfolio/src/components/InfoCard.component.tsx
+++ portfolio/src/components/InfoCard.component.tsx
@@ -26,7 +26,7 @@ function InfoCard(inforCardProps: InfoCardProps) {
     <div>
       <Link to={`/project/${inforCardProps.Id}`}>
         <Card
-          className="h-32 w-unit-9xl bg-secondary"
+          className="h-32 w-unit-8xl bg-secondary"
           isBlurred
           isHoverable
           isPressable
@@ -36,27 +36,27 @@ function InfoCard(inforCardProps: InfoCardProps) {
               <Image
                 src="https://placehold.co/200"
                 alt="Logo"
-                className=" h-24 w-24 rounded-full col-span-3 col"
+                className=" h-24 w-24 rounded-full"
               />
             </div>
             <div className="flex justify-center col-span-10 row-start-1">
               <p className="text-background">{inforCardProps.title}</p>
             </div>
-            <div className="col-span-4  col-start-3 row-start-3 flex flex-row gap-1">
+            <div className="col-span-4  col-start-4 row-start-3 flex flex-row gap-1">
               {inforCardProps.Language ? (
                 <Chip color="primary" className="rounded-full">
                   {inforCardProps.Language}
                 </Chip>
               ) : null}
             </div>
-            <div className="flex flex-row col-span-7 col-start-5 row-start-3">
+            <div className="flex flex-row col-span-7 col-start-7 row-start-3 gap-1">
               {inforCardProps.category.map((category) => (
-                <Chip color="primary" className="rounded-full">
+                <Chip key={category} color="primary" className="rounded-full">
                   {category}
                 </Chip>
               ))}
             </div>
-            <div className="col-span-10 row-start-5 col-start-3 py-3">
+            <div className="col-span-7 row-start-5 col-start-3 py-3 ">
               <p className="text-background">{inforCardProps.description}</p>
             </div>
           </CardBody>
